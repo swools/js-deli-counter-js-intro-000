b94bf6cfe71f3375dde9069138836b4c47b23a63
Stephen Wool <stevewool7@gmail.com>
2019-03-05 01:38:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
     return `The line is currently empty`
   } else {
     for (let i = 0; i < line.length; i++) {
-      line.push(`${i + 1}`)
+      line.push(`${i + 1}. ${line[i]}`)
     }
   }
 }
