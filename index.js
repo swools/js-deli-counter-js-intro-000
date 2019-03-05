87949611a1667220617c91269a380ac1a5d1abe9
Stephen Wool <stevewool7@gmail.com>
2019-03-05 01:37:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function nowServing(line) {
     return `Currently serving ${line.shift()}.`
   }
 }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return `The line is currently empty`
+  } else {
+    for (let i = 0; i < line.length; i++) {
+      return ``
+    }
+  }
+}
